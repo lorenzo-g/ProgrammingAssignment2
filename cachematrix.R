0eecb97937366529adbdf92f9ee4fe3f0b926285
lorenzo-g <lorenzo.guizzaro@mail.com>
2014-04-24 00:55:00 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# lorenzo-g here, glad to meet you!
+# this one was for week2 assignment from rprog002
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  # an empty variable created, ready to be filled.
+  
+  set <- function(matrix2) {
+    x <<- matrix2
+    inverse <<- NULL
+  }
+  # set (or re-set) inverse to "NULL"
+  
+  get <- function() {
+    print(x)
+  }
+  # just asking, what did I say for X? ; )
+  
+  makeinverse <- function(x) {
+    inverse <<- solve(x)
+  }
+  # makes the operation requested on the matrix
+  
+  getinverse <- function() {
+    print(inverse)
+  }
+  # prints the inverse matrix
+  
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       makeinverse = makeinverse)
+  
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  if (x$getinverse != NULL) {
+    #if inverse is not empty
+    inverse <- x$makeinverse
+    #uses the makeinverse function from above
+  }
+  # else... nothing happens!
+  print (inverse)
 }
